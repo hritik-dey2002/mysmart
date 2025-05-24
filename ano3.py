0eb8d99c560cab1d79c15f79b5186a9fa6d7688e
Hritik Dey <110844175+hritik-dey2002@users.noreply.github.com>
2025-05-24 23:01:00 +0530

    Update ano3.py

--- ano3.py
+++ ano3.py
@@ -27,63 +27,6 @@ from streamlit_option_menu import option_menu
 from streamlit_navigation_bar import st_navbar
 
 
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.imghippo.com/files/5UcL11724915435.png");
-
-background-position: center center;
-
-/* Make image fixed */
-background-attachment: fixed;
-
-/* Not repeat images */
-background-repeat: no-repeat;
-
-/* Set background size auto */
-background-size: 100%;
-}}
-
-
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-</style>
-"""
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.imghippo.com/files/5UcL11724915435.png");
-
-background-position: center center;
-
-/* Make image fixed */
-background-attachment: fixed;
-
-/* Not repeat images */
-background-repeat: no-repeat;
-
-/* Set background size auto */
-background-size: 100%;
-}}
-
-
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-</style>
-"""
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 
 # Function to generate a random meeting link
